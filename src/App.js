b6eec5e4caed609b7ca4311f91237db274f4609e
ruldane <l.nizzoli@gmail.com>
2021-02-16 07:32:35 -0800

    add projectTask & projectBoard

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import AddProject from "./components/Project/AddProject";
 import UpdateProject from "./components/Project/UpdateProject";
 import { Provider } from "react-redux";
 import store from "./store";
+import ProjectBoard from "./components/projectBoard/ProjectBoard";
+import AddProjectTask from "./components/projectBoard/ProjectTasks/AddProjectTask";
 
 class App extends Component {
     render() {
@@ -19,6 +21,8 @@ class App extends Component {
                         <Route exact path="/dashboard" component={Dashboard} />
                         <Route exact path="/addProject" component={AddProject} />
                         <Route exact path="/updateProject/:id" component={UpdateProject} />
+                        <Route exact path="/projectBoard/:id" component={ProjectBoard}/>
+                        <Route exact path="/addProjectTask/:id" component={AddProjectTask}/>
                     </div>
                 </Router>
             </Provider>
---
+++ src/actions/backlogActions.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import {GET_ERRORS, GET_BACKLOG} from "./types";
+
+export const addProjectTask = (backlog_id, project_task, history) => async dispatch => {
+    try {
+        await axios.post(`/api/backlog/${backlog_id}`, project_task)
+        history.push(`/projectBoard/${backlog_id}`);
+        dispatch({
+            type: GET_ERRORS,
+            payload: {}
+        });
+    }catch (e){
+        dispatch({
+            type: GET_ERRORS,
+            payload: e.response.data
+        });
+    }
+}
+
+export const getBacklog = (backlog_id) => async dispatch => {
+    try {
+        const res = await axios.get(`/api/backlog/${backlog_id}`)
+        dispatch ({
+            type: GET_BACKLOG,
+            payload: res.data
+        })
+    } catch (e){
+
+    }
+}
